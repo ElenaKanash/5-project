bc12bdb43b703e45d87aae0f084de436efa62398
Elena Kanash <hird250@gmail.com>
2022-10-20 14:00:48 +0300

    feat: add active вефке slide

--- slider/app.js
+++ slider/app.js
@@ -1,14 +1,24 @@
-const slides = document.querySelectorAll('.slide');
+function slidesPlugin(activeSlide = 0) {
 
-slides.forEach((slide) => {
-  slide.addEventListener('click', () => {
-    clearActiveClasses();
-    slide.classList.add('active');
-  });
-});
+  const slides = document.querySelectorAll('.slide');
 
-function clearActiveClasses() {
+  //choose active slide with start
+  slides[activeSlide].classList.add('active');
+
   slides.forEach((slide) => {
-    slide.classList.remove('active');
+    slide.addEventListener('click', () => {
+      clearActiveClasses();
+      slide.classList.add('active');
+    });
   });
+
+  function clearActiveClasses() {
+    slides.forEach((slide) => {
+      slide.classList.remove('active');
+    });
+  }
 }
+
+slidesPlugin(4)
+
+
